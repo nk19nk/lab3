562cb98f86c9cfb4dc6566201ac9b9bba8cf8df6
nasty <nastya_aa19@mail.ru>
2024-11-10 07:11:54 +0500

    1

---
+++ src/Fraction4.java
@@ -0,0 +1,92 @@
+public final class Fraction4 extends Number {
+    // Поля
+    private final int chisl;
+    private final int znam;
+
+    // Конструктор
+    public Fraction4(int chisl, int znam) {
+        if (znam == 0) {
+            throw new IllegalArgumentException("Знаменатель не может быть равен нулю");
+        }
+        if (znam < 0) {
+            this.chisl = -chisl;
+            this.znam = -znam;
+        } else {
+            this.chisl = chisl;
+            this.znam = znam;
+        }
+    }
+
+    // Геттеры
+    public int getChisl() {
+        return this.chisl;
+    }
+
+    public int getZnam() {
+        return this.znam;
+    }
+
+    // Методы
+    @Override
+    public String toString() {
+        return chisl + "/" + znam;
+    }
+
+    public Fraction4 sum(Fraction4 other) {
+        int newchisl = this.chisl * other.znam + other.chisl * this.znam;
+        int newznam = this.znam * other.znam;
+        return new Fraction4(newchisl, newznam);
+    }
+
+    public Fraction4 minus(Fraction4 other) {
+        int newchisl = this.chisl * other.znam - other.chisl * this.znam;
+        int newznam = this.znam * other.znam;
+        return new Fraction4(newchisl, newznam);
+    }
+
+    public Fraction4 multiply(Fraction4 other) {
+        int newchisl = this.chisl * other.chisl;
+        int newznam = this.znam * other.znam;
+        return new Fraction4(newchisl, newznam);
+    }
+
+    public Fraction4 div(Fraction4 other) {
+        if (other.chisl == 0) {
+            throw new ArithmeticException("Деление на ноль");
+        }
+        int newchisl = this.chisl * other.znam;
+        int newznam = this.znam * other.chisl;
+        return new Fraction4(newchisl, newznam);
+    }
+
+    // Реализация методов класса Number
+    @Override
+    public int intValue() {
+        return chisl / znam;
+    }
+
+    @Override
+    public long longValue() {
+        return (long) chisl / znam;
+    }
+
+    @Override
+    public float floatValue() {
+        return (float) chisl / znam;
+    }
+
+    @Override
+    public double doubleValue() {
+        return (double) chisl / znam;
+    }
+
+    @Override
+    public byte byteValue() {
+        return (byte) (chisl / znam);
+    }
+
+    @Override
+    public short shortValue() {
+        return (short) (chisl / znam);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("Задание 1.4");
         System.out.println("Сколько дробей вы хотите создать?");
         int col = in.nextInt();
         List<Fraction> fractions = new ArrayList<>();
@@ -31,63 +32,242 @@ public class Main {
             System.out.println("Вычитание - 2");
             System.out.println("Умножение - 3");
             System.out.println("Деление - 4");
-            System.out.println("Выход - 5");
+            System.out.println("Выход - 0");
             System.out.print("Введите номер операции: ");
             int oper = in.nextInt();
 
-            if (oper == 5) {
+            if (oper == 0) {
                 //System.out.println("Выход из программы.");
                 break;
             }
-
-            System.out.println("Список введенных дробей:");
-            for (int i = 0; i < fractions.size(); i++) {
-                System.out.println((i + 1) + ". " + fractions.get(i));
-            }
-
-            System.out.print("Выберите первую дробь из " + fractions.size() + " дробей(напишите ее номер(1-"+ fractions.size() +"):");
-            int nomer1 = in.nextInt() - 1;
-            if (nomer1 < 0 || nomer1 >= fractions.size()) {
-                System.out.println("Некорректный выбор дроби. Попробуйте снова.");
-                continue;
-            }
-            Fraction f1 = fractions.get(nomer1);
-
-            System.out.println("Список введенных дробей:");
-            for (int i = 0; i < fractions.size(); i++) {
-                System.out.println((i + 1) + ". " + fractions.get(i));
-            }
-
+            Fraction f1 = null;
             Fraction f2 = null;
+
             if (oper >= 1 && oper <= 4) {
-                System.out.print("Выберите вторую дробь из " + fractions.size() + " дробей(напишите ее номер(1-"+ fractions.size() +"):");
+
+                System.out.println("Список введенных дробей:");
+                for (int i = 0; i < fractions.size(); i++) {
+                    System.out.println((i + 1) + ") " + fractions.get(i));
+                }
+
+                System.out.print("Выберите первую дробь из " + fractions.size() + " дробей(напишите ее номер(1-" + fractions.size() + "):");
+                int nomer1 = in.nextInt() - 1;
+                if (nomer1 < 0 || nomer1 >= fractions.size()) {
+                    System.out.println("Дроби с таким номером нет, введите снова");
+                    continue;
+                }
+                f1 = fractions.get(nomer1);
+
+                System.out.println("Список введенных дробей:");
+                for (int i = 0; i < fractions.size(); i++) {
+                    System.out.println((i + 1) + ". " + fractions.get(i));
+                }
+
+                System.out.print("Выберите вторую дробь из " + fractions.size() + " дробей(напишите ее номер(1-" + fractions.size() + "):");
                 int nomer2 = in.nextInt() - 1;
                 if (nomer2 < 0 || nomer2 >= fractions.size()) {
-                    System.out.println("Некорректный выбор дроби. Попробуйте снова.");
+                    System.out.println("Дроби с таким номером нет, введите снова");
                     continue;
                 }
                 f2 = fractions.get(nomer2);
             }
-
             switch (oper) {
                 case 1: // Сложение
-                    System.out.println("Результат сложения: " + f1.sum(f2));
+                    System.out.println(f1 + " + " + f2 + " = " + f1.sum(f2));
                     break;
                 case 2: // Вычитание
-                    System.out.println("Результат вычитания: " + f1.minus(f2));
+                    System.out.println(f1 + " - " + f2 + " = " + f1.minus(f2));
                     break;
                 case 3: // Умножение
-                    System.out.println("Результат умножения: " + f1.multiply(f2));
+                    System.out.println(f1 + " * " + f2 + " = " + f1.multiply(f2));
                     break;
                 case 4: // Деление
                     try {
-                        System.out.println("Результат деления: " + f1.div(f2));
+                        System.out.println(f1 + " / " + f2 + " = " + f1.div(f2));
                     } catch (ArithmeticException e) {
                         System.out.println("Ошибка: " + e.getMessage());
                     }
                     break;
                 default:
-                    System.out.println("Некорректный выбор операции. Попробуйте снова.");
+                    System.out.println("Операции с таким номером нет, введите снова");
+                    break;
+            }
+        }
+
+
+        System.out.println("Задание 3.1");
+        System.out.println("Сколько дробей вы хотите создать?");
+        int colv = in.nextInt();
+        List<Fraction3> fractions3 = new ArrayList<>();
+        for (int i = 0; i < colv; i++) {
+            System.out.println("Введите " + (i + 1) + " дробь");
+            System.out.print("числитель: ");
+            int ch3 = in.nextInt();
+            System.out.print("знаменатель: ");
+            int zn3 = in.nextInt();
+            try {
+                Fraction3 f3 = new Fraction3(ch3, zn3);
+                fractions3.add(f3);
+                System.out.println("Введенная дробь: " + f3);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ошибка: " + e.getMessage());
+                i--; // Уменьшаем счетчик, чтобы повторить ввод для этой дроби
+            }
+        }
+        // Пример использования дробей для операций
+        while (true) {
+            System.out.println("Выберите операцию:");
+            System.out.println("Сложение - 1");
+            System.out.println("Вычитание - 2");
+            System.out.println("Умножение - 3");
+            System.out.println("Деление - 4");
+            System.out.println("Выход - 0");
+            System.out.print("Введите номер операции: ");
+            int oper3 = in.nextInt();
+
+            if (oper3 == 0) {
+                //System.out.println("Выход из программы.");
+                break;
+            }
+            Fraction3 frac1 = null;
+            Fraction3 frac2 = null;
+
+            if (oper3 >= 1 && oper3 <= 4) {
+
+                System.out.println("Список введенных дробей:");
+                for (int i = 0; i < fractions3.size(); i++) {
+                    System.out.println((i + 1) + ") " + fractions3.get(i));
+                }
+
+                System.out.print("Выберите первую дробь из " + fractions3.size() + " дробей(напишите ее номер(1-" + fractions3.size() + "):");
+                int nom1 = in.nextInt() - 1;
+                if (nom1 < 0 || nom1 >= fractions3.size()) {
+                    System.out.println("Дроби с таким номером нет, введите снова");
+                    continue;
+                }
+                frac1 = fractions3.get(nom1);
+
+                System.out.println("Список введенных дробей:");
+                for (int i = 0; i < fractions3.size(); i++) {
+                    System.out.println((i + 1) + ". " + fractions3.get(i));
+                }
+
+                System.out.print("Выберите вторую дробь из " + fractions3.size() + " дробей(напишите ее номер(1-" + fractions3.size() + "):");
+                int nom2 = in.nextInt() - 1;
+                if (nom2 < 0 || nom2 >= fractions3.size()) {
+                    System.out.println("Дроби с таким номером нет, введите снова");
+                    continue;
+                }
+                frac2 = fractions3.get(nom2);
+            }
+            switch (oper3) {
+                case 1: // Сложение
+                    System.out.println(frac1 + " + " + frac2 + " = " + frac1.sum(frac2));
+                    break;
+                case 2: // Вычитание
+                    System.out.println(frac1 + " - " + frac2 + " = " + frac1.minus(frac2));
+                    break;
+                case 3: // Умножение
+                    System.out.println(frac1 + " * " + frac2 + " = " + frac1.multiply(frac2));
+                    break;
+                case 4: // Деление
+                    try {
+                        System.out.println(frac1 + " / " + frac2 + " = " + frac1.div(frac2));
+                    } catch (ArithmeticException e) {
+                        System.out.println("Ошибка: " + e.getMessage());
+                    }
+                    break;
+                default:
+                    System.out.println("Операции с таким номером нет, введите снова");
+                    break;
+            }
+        }
+
+
+        System.out.println("Задание 4.2");
+        System.out.println("Сколько дробей вы хотите создать?");
+        int colvo = in.nextInt();
+        List<Fraction4> fractions4 = new ArrayList<>();
+        for (int i = 0; i < colvo; i++) {
+            System.out.println("Введите " + (i + 1) + " дробь");
+            System.out.print("числитель: ");
+            int ch4 = in.nextInt();
+            System.out.print("знаменатель: ");
+            int zn4 = in.nextInt();
+            try {
+                Fraction4 f4 = new Fraction4(ch4, zn4);
+                fractions4.add(f4);
+                System.out.println("Введенная дробь: " + f4);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ошибка: " + e.getMessage());
+                i--; // Уменьшаем счетчик, чтобы повторить ввод для этой дроби
+            }
+        }
+        // Пример использования дробей для операций
+        while (true) {
+            System.out.println("Выберите операцию:");
+            System.out.println("Сложение - 1");
+            System.out.println("Вычитание - 2");
+            System.out.println("Умножение - 3");
+            System.out.println("Деление - 4");
+            System.out.println("Выход - 0");
+            System.out.print("Введите номер операции: ");
+            int oper4 = in.nextInt();
+
+            if (oper4 == 0) {
+                //System.out.println("Выход из программы.");
+                break;
+            }
+            Fraction4 fract1 = null;
+            Fraction4 fract2 = null;
+
+            if (oper4 >= 1 && oper4 <= 4) {
+
+                System.out.println("Список введенных дробей:");
+                for (int i = 0; i < fractions4.size(); i++) {
+                    System.out.println((i + 1) + ") " + fractions4.get(i));
+                }
+
+                System.out.print("Выберите первую дробь из " + fractions4.size() + " дробей(напишите ее номер(1-" + fractions4.size() + "):");
+                int n1 = in.nextInt() - 1;
+                if (n1 < 0 || n1 >= fractions4.size()) {
+                    System.out.println("Дроби с таким номером нет, введите снова");
+                    continue;
+                }
+                fract1 = fractions4.get(n1);
+
+                System.out.println("Список введенных дробей:");
+                for (int i = 0; i < fractions4.size(); i++) {
+                    System.out.println((i + 1) + ". " + fractions4.get(i));
+                }
+
+                System.out.print("Выберите вторую дробь из " + fractions4.size() + " дробей(напишите ее номер(1-" + fractions4.size() + "):");
+                int n2 = in.nextInt() - 1;
+                if (n2 < 0 || n2 >= fractions4.size()) {
+                    System.out.println("Дроби с таким номером нет, введите снова");
+                    continue;
+                }
+                fract2 = fractions4.get(n2);
+            }
+            switch (oper4) {
+                case 1: // Сложение
+                    System.out.println(fract1 + " + " + fract2 + " = " + fract1.sum(fract2));
+                    break;
+                case 2: // Вычитание
+                    System.out.println(fract1 + " - " + fract2 + " = " + fract1.minus(fract2));
+                    break;
+                case 3: // Умножение
+                    System.out.println(fract1 + " * " + fract2 + " = " + fract1.multiply(fract2));
+                    break;
+                case 4: // Деление
+                    try {
+                        System.out.println(fract1 + " / " + fract2 + " = " + fract1.div(fract2));
+                    } catch (ArithmeticException e) {
+                        System.out.println("Ошибка: " + e.getMessage());
+                    }
+                    break;
+                default:
+                    System.out.println("Операции с таким номером нет, введите снова");
                     break;
             }
         }
